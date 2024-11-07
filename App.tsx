6cebc3aeff6d961400b7a39c9ec8ad1917c50461
Jakub Kasprzyk <kasprzykjakub@proton.me>
2024-11-07 23:21:56 +0100

    chore: update npm dependencies

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  <View style={s.container}>
+    <Text>App</Text>
+  </View>;
+};
+
+export default App;
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
